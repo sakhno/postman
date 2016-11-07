5c3adbac9f2da98851f65468baf29546f0ccb20c
Anton <sakhno83@gmail.com>
2016-11-07 21:44:47 +0200

    main page controller fix

--- controller/src/main/java/com/postman/controllers/MainPageController.java
+++ controller/src/main/java/com/postman/controllers/MainPageController.java
@@ -41,7 +41,7 @@ public class MainPageController {
 
     @RequestMapping("/home")
     public String homePage(Model model, HttpServletRequest request, Principal principal) {
-        notifyMyAboutVisitor(request);
+//        notifyMyAboutVisitor(request);
         model.addAttribute("user", getCurrentUser(principal));
         model.addAttribute("searchForm", new SearchForm());
         return "home";
@@ -114,7 +114,6 @@ public class MainPageController {
 
     private boolean notifyIP(String ip, String server) {
         if("localhost".equals(server))return false;
-        
         return !ip.matches("(176.104.50.246)|(69.162.124.237)|(66.249.64..*)");
     }
 }
